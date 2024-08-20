483bc3561f019a03dd1aaef5c2697edf324a4708
ChrisWudel <chris-wudel@web.de>
2024-08-20 12:45:27 +0200

    Update setup.R

--- func/setup.R
+++ func/setup.R
@@ -1,29 +1,3 @@
-#' setup
-#'
-#' @description Sets up the R environment by installing and loading specified packages and functions.
-#'
-#' @param packages A character vector of package names to be installed and loaded.
-#' @param func_names A character vector of function names to be loaded from external scripts.
-#'
-#' @details
-#' This function checks if the specified packages are already installed, automatically 
-#' installs any missing packages, and then loads all specified packages into the current R session.
-#' It also downloads and sources external R scripts to load additional functions.
-#'
-#' @return NULL
-#'
-#' @aliases setup
-#' @rdname setup
-#'
-#' @keywords internal
-#' 
-# Load the necessary library for quiet downloading
-if (!requireNamespace("curl", quietly = TRUE)) {
-  install.packages("curl")
-}
-
-library(curl)
-
 setup <- function(packages, func_names) {
   # Define URLs for the R scripts
   urls <- list(
@@ -41,11 +15,14 @@ setup <- function(packages, func_names) {
     
     temp_file <- tempfile(fileext = ".R")
     tryCatch({
-      # Use curl to download the file quietly
-      curl_download(url, temp_file, quiet = TRUE)
+      # Use a temporary file to suppress download progress
+      download.file(url, temp_file, quiet = TRUE)
+      
       if (file.exists(temp_file)) {
-        # Suppress messages from source
-        suppressMessages(suppressWarnings(source(temp_file, local = FALSE)))
+        # Suppress messages and warnings from source
+        suppressMessages(suppressWarnings({
+          source(temp_file, local = FALSE)
+        }))
         unlink(temp_file)
       } else {
         stop("Failed to download script from ", url)
